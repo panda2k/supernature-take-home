eeabe8fdb8c158f6c71e137b129d818456977b8e
panda2k <thelowkeypanda@gmail.com>
2023-01-23 10:44:02 -0800

    Change resizing on screen sizes

--- src/components/ViewUploaded.vue
+++ src/components/ViewUploaded.vue
@@ -4,31 +4,41 @@
         Past Uploads
     </v-btn>
     <v-dialog v-model="popup" class="">
-        <v-card class="d-flex flex-col align-center py-6 px-6">
-            <div v-if="files.length == 0">
-                {{ this.loading ? "Loading..." : "No files uploaded" }}
-            </div>
-            <div v-for="file in files" :key="file.name + file.timestamp" class="d-flex justify-space-between w-100 py-3">
-                <div style="margin-top: 6px;">
-                    {{ file.name }}
-                </div>
-                <div class="d-flex flex-row-reverse">
-                    <v-btn @click="load(file)" class="ml-3">
-                        View
-                    </v-btn>
-                    <v-btn @click="remove(file)" class="ml-3">
-                       Delete 
-                    </v-btn>
-                    <v-btn @click="download(file)" class="ml-3">
-                        Download 
-                    </v-btn>
+        <div class="d-flex justify-center w-100 align-center">
 
+            <v-card 
+                class="d-flex flex-col align-center py-6 px-6" 
+                :class="{
+                    'w-50': lgAndUp && (this.loading || this.files.length == 0),
+                    'w-75': md && (this.loading || this.files.length == 0),
+                    'w-100': !mdAndUp || (!this.loading && this.files.length != 0)
+                }"
+            >
+                <div v-if="files.length == 0" class="text-center w-100">
+                    {{ this.loading ? "Loading..." : "No files uploaded" }}
+                </div>
+                <div v-for="file in files" :key="file.name + file.timestamp" class="d-flex justify-space-between w-100 py-3">
                     <div style="margin-top: 6px;">
-                        {{ file.timestamp.toLocaleDateString("en-US") + " " + file.timestamp.toLocaleTimeString() }}
+                        {{ file.name }}
+                    </div>
+                    <div class="d-flex flex-row-reverse">
+                        <v-btn @click="load(file)" class="ml-3">
+                            View
+                        </v-btn>
+                        <v-btn @click="remove(file)" class="ml-3">
+                           Delete 
+                        </v-btn>
+                        <v-btn @click="download(file)" class="ml-3">
+                            Download 
+                        </v-btn>
+
+                        <div style="margin-top: 6px;">
+                            {{ file.timestamp.toLocaleDateString("en-US") + " " + file.timestamp.toLocaleTimeString() }}
+                        </div>
                     </div>
                 </div>
-            </div>
-        </v-card>
+            </v-card>
+        </div>
     </v-dialog>
 </template>
 
@@ -36,10 +46,16 @@
 import { getStorage, ref, listAll, deleteObject, getBlob } from "firebase/storage"
 import { firebaseApp } from "../firebase"
 import readXlsxFile from 'read-excel-file'
+import { useDisplay } from 'vuetify'
 
 const storage = getStorage(firebaseApp)
 
 export default {
+    setup() {
+        const { md, mdAndUp, lgAndUp } = useDisplay()
+
+        return { md, mdAndUp, lgAndUp }
+    },
     data() {
         return {
             popup: false,
@@ -49,11 +65,14 @@ export default {
     },
     methods: {
         fetchFiles() {
-            this.loading = true;
+            this.loading = true
             this.files = []
             const listRef = ref(storage, `${this.$store.state.user.uid}`)
             listAll(listRef)
                 .then(res => {
+                    if (res.prefixes.length == 0) {
+                        this.loading = false
+                    }
                     res.prefixes.forEach(ref => {
                         listAll(ref)
                             .then(result => {
@@ -65,13 +84,13 @@ export default {
                                     })
                                 })
                             })
+                            .finally(() => {
+                                this.loading = false
+                            })
                     })
                 })
                 .catch(error => {
                     console.log(error)
-                })
-                .finally(() => {
-                    this.loading = false
                 })
         },
         async load (file) {
